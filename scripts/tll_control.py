764a95411622f16a95179c72e8000d5913a523e4
Daniel Ruiz <dacruizac@unal.edu.co>
2020-11-06 00:36:25 -0500

    Segunda version del codigo

--- scripts/tll_control.py
+++ scripts/tll_control.py
@@ -2,6 +2,7 @@
 
 from __future__ import print_function
 import rospy
+from std_msgs.msg import Bool
 import numpy as np
 from   rospy.numpy_msg import numpy_msg
 from tf.transformations import rotation_matrix 
@@ -11,10 +12,55 @@ import tf_conversions
 
 from geometry_msgs.msg import TransformStamped,Twist
 from pid_control import controller
-#from ar_track_alvar_msgs.msg import AlvarMarkers, AlvarMarker
 
 class control_node(object):
     def __init__(self):
+        self.init_vars()
+
+        self.rate=rospy.Rate(10)
+
+        self.init_first_TM(np.deg2rad(-90))
+
+
+        if (not self.initial_calibration()):
+            return
+        
+        self.calc_odom_transf()
+
+        while (not rospy.is_shutdown() and (np.linalg.norm(self.actual_pos-self.goal)>0.1 or len(self.point_set)>0)):
+            self.get_odom()
+            
+            self.calc_odom_transf()
+            
+            if (np.linalg.norm(self.actual_pos-self.goal)<0.1 and len(self.point_set)>0):
+                self.goal=np.array([self.point_set.pop(0)],dtype=np.float)
+                self.goal=np.transpose(self.goal)
+            
+            pos_err=self.goal - self.actual_pos
+            err_in_base=np.matmul(self.rot_origin_2_base,pos_err)
+
+            print("error en base", err_in_base)
+
+            ang_err=np.arctan2(err_in_base[1],err_in_base[0])[0]
+            print("Posicion actual ",end="")
+            print(self.actual_pos)
+            print("Error de posicion ",end="")
+            print(np.linalg.norm(pos_err))
+            print("Error angulo ",end="")
+            print(ang_err)
+            print(self.original_fr)
+
+            self.control_conditional_action(ang_err,pos_err)
+
+            self.rate.sleep()
+
+    def init_callback(self,msg):
+        self.init_flag=msg.data
+
+    def init_vars(self):
+        self.init_flag=False
+        self.actual_pos=np.zeros((2,1),dtype=np.float)
+        rospy.Subscriber("tll2/begin_control",Bool,callback=self.init_callback,queue_size=10)
         self.controllers=controller(0.4,1.0,0.3,np.pi/3)
         self.point_set=[[0, 0], [-3.5, 0], [-3.5, 3.5], [1.5, 3.5], [1.5, -1.5], [3.5, -1.5]]
         self.point_set+=[[3.5, -8.0], [-2.5, -8.0], [-2.5, -5.5], [1.5, -5.5], [1.5, -3.5],[-1.0, -3.5]]
@@ -28,101 +74,68 @@ class control_node(object):
         self.goal=np.array([self.point_set.pop(0)],dtype=np.float)
         self.goal=np.transpose(self.goal)
 
-        i=0
-        rate=rospy.Rate(10)
+        self.velocity_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
+        self.tw_msg=Twist()
 
-        self.original_fr=np.zeros((4,4),dtype=np.float)
-        self.original_fr[3,3]=1
-        self.original_fr[2,2]=1
-        self.original_fr[1,0]=-1
-        self.original_fr[0,1]=1
+    def init_first_TM(self,angle):
+        self.original_fr=np.identity(4,dtype=np.float)
+        rot=[[np.cos(angle),-np.sin(angle)],[np.sin(angle),np.cos(angle)]]
+        self.original_fr[0:2,0:2]=np.array(rot,dtype=np.float)
         print(self.original_fr)
 
+    def initial_calibration(self):
+        i=0
         while i<10:
             try:
-                trans_odom = self.tfBuffer.lookup_transform("base_footprint","odom", rospy.Time())
+                self.trans_odom = self.tfBuffer.lookup_transform("odom","base_footprint", rospy.Time())
                 i=15
             except:
                 i+=1
-                rate.sleep()
+                self.rate.sleep()
                 rospy.logwarn("Error trying to look for transform")
         if (i<15):
-            return
+            return False
+        return True
 
-        self.quat = np.array([trans_odom.transform.rotation.x, \
-                                    trans_odom.transform.rotation.y, \
-                                    trans_odom.transform.rotation.z, \
-                                    trans_odom.transform.rotation.w])
+    def calc_odom_transf(self):
+        self.quat = np.array([self.trans_odom.transform.rotation.x, \
+                                    self.trans_odom.transform.rotation.y, \
+                                    self.trans_odom.transform.rotation.z, \
+                                    self.trans_odom.transform.rotation.w])
         self.rot_matr_base_odom = tf_conversions.transformations.quaternion_matrix(self.quat)
         self.rot_matr_base_odom = self.rot_matr_base_odom.copy()
-        self.rot_matr_base_odom[0,3]=trans_odom.transform.translation.x
-        self.rot_matr_base_odom[1,3]=trans_odom.transform.translation.y
-        self.rot_matr_base_odom[2,3]=trans_odom.transform.translation.z
+        self.rot_matr_base_odom[0,3]=self.trans_odom.transform.translation.x
+        self.rot_matr_base_odom[1,3]=self.trans_odom.transform.translation.y
+        self.rot_matr_base_odom[2,3]=self.trans_odom.transform.translation.z
 
         self.base_2_origin= np.matmul(self.original_fr,self.rot_matr_base_odom)
         
-        self.actual_pos=np.zeros((2,1),dtype=np.float)
+        self.rot_origin_2_base = self.base_2_origin.copy()[0:2,0:2]
+        self.rot_origin_2_base=np.transpose(self.rot_origin_2_base)
+
         self.actual_pos[:,0]=self.base_2_origin[0:2,3]
 
-        self.velocity_pub = rospy.Publisher("/mobile_base/commands/velocity", Twist, queue_size=10)
-        self.tw_msg=Twist()
-        pos_err_ext=np.zeros((4,1),dtype=np.float)
-        pos_err_ext[3]=1
-        while (not rospy.is_shutdown() and (np.linalg.norm(self.actual_pos-self.goal)>0.1 or len(self.point_set)>0)):
-            #rospy.loginfo("bien")
-            try:
-                trans_odom = self.tfBuffer.lookup_transform("odom","base_footprint", rospy.Time())
-            except:
-                rospy.logwarn("Error trying to look for transform")
-                #return 
-            
-            self.quat = np.array([trans_odom.transform.rotation.x, \
-                                    trans_odom.transform.rotation.y, \
-                                    trans_odom.transform.rotation.z, \
-                                    trans_odom.transform.rotation.w])
-            self.rot_matr_base_odom = tf_conversions.transformations.quaternion_matrix(self.quat)
-            self.rot_matr_base_odom = self.rot_matr_base_odom.copy()
-            self.rot_matr_base_odom[0,3]=trans_odom.transform.translation.x
-            self.rot_matr_base_odom[1,3]=trans_odom.transform.translation.y
-            self.rot_matr_base_odom[2,3]=trans_odom.transform.translation.z
+    def get_odom(self):
+        try:
+            self.trans_odom = self.tfBuffer.lookup_transform("odom","base_footprint", rospy.Time())
+        except:
+            rospy.logwarn("Error trying to look for transform")
 
-            self.base_2_origin = np.matmul(self.original_fr,self.rot_matr_base_odom)
-            rot_origin_2_base = self.base_2_origin.copy()[0:2,0:2]
-            rot_origin_2_base=np.transpose(rot_origin_2_base)
-            print(rot_origin_2_base)
+    def control_conditional_action(self,ang_err,pos_err):
+        self.tw_msg.linear.x=0
+        self.tw_msg.angular.z=0
+        ang_cmd=self.controllers.angular_control(ang_err)
+        print("ang_cmd",ang_cmd)
+        self.tw_msg.angular.z=ang_cmd
+        if (abs(ang_err)<0.01):
+            vel_cmd=self.controllers.linear_control(np.linalg.norm(pos_err))
+            self.tw_msg.linear.x=vel_cmd
+        self.velocity_pub.publish(self.tw_msg)
 
-            self.actual_pos[:,0]=self.base_2_origin[0:2,3]
-            if (np.linalg.norm(self.actual_pos-self.goal)<0.1 and len(self.point_set)>0):
-                #(np.linalg.norm(self.actual_pos-self.goal)<0.6 and len(self.point_set)>0):
-                #(np.linalg.norm(self.actual_pos-self.goal)<0.1 and len(self.point_set)>0):
-                self.goal=np.array([self.point_set.pop(0)],dtype=np.float)
-                self.goal=np.transpose(self.goal)
-            
-            pos_err=self.goal - self.actual_pos
-            #pos_err_ext[0:2,0]=pos_err[:,0]
-            err_in_base=np.matmul(rot_origin_2_base,pos_err)
-
-            ang_err=np.arctan2(err_in_base[1],err_in_base[0])[0]
-            print("Posicion actual ",end="")
-            print(self.actual_pos)
-            print("Error de posicion ",end="")
-            print(np.linalg.norm(pos_err))
-            print("Error angulo ",end="")
-            print(ang_err)
-            self.tw_msg.linear.x=0
-            self.tw_msg.angular.z=0
-            ang_cmd=self.controllers.angular_control(ang_err)
-            #self.tw_msg.linear.x=0
-            self.tw_msg.angular.z=ang_cmd
-            #self.velocity_pub.publish(self.tw_msg)
-            if (abs(ang_err)<0.01):
-                vel_cmd=self.controllers.linear_control(np.linalg.norm(pos_err))
-                #print(vel_cmd)
-                self.tw_msg.linear.x=vel_cmd
-                #self.tw_msg.angular.z=0
-                #self.velocity_pub.publish(self.tw_msg)
-            self.velocity_pub.publish(self.tw_msg)
-            rate.sleep()
-            #if(i>=20):
-            #    break
-            #i+=1
+    def control_complete_action(self,ang_err,pos_err):
+        self.tw_msg.linear.x=0
+        self.tw_msg.angular.z=0
+        ang_cmd=self.controllers.angular_control(ang_err)
+        self.tw_msg.angular.z=ang_cmd
+        vel_cmd=self.controllers.linear_control(np.linalg.norm(pos_err))
+        self.tw_msg.linear.x=vel_cmd
